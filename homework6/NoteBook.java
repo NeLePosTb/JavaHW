f223bfe96146d0bc01940b447c1bc5721469c2f2
Vlad <vmereskin@gmail.com>
2023-07-04 00:30:21 +0300

    add task5

---
+++ homework6/NoteBook.java
@@ -0,0 +1,57 @@
+package homework6;
+
+public class NoteBook { 
+      private String model;
+    private Integer ram;
+    private Integer storage;
+    private String os;
+    private String color;
+    public NoteBook(String model, int ram, int storage, String os, String color) {
+        this.model = model;
+        this.ram = ram;
+        this.storage = storage;
+        this.os = os;
+        this.color = color;
+    }
+    // Геттеры и сеттеры для полей
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+    public int getStorage() {
+        return storage;
+    }
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+    public String getOs() {
+        return os;
+    }
+    public void setOs(String os) {
+        this.os = os;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    @Override
+    public String toString() {
+        return "NoteBook{" +
+                "model='" + model + '\'' +
+                ", ram=" + ram +
+                ", storage=" + storage +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
